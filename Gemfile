45f02ad496d1ef732172a632f3784b72a6117df2
heel <heel@comp.(none)>
2010-12-14 01:12:54 +0200

    Rspec

--- Gemfile
+++ Gemfile
@@ -7,6 +7,10 @@ gem 'rails', '3.0.3'
 
 gem 'mysql2'
 
+group :test, :development do
+  gem "rspec-rails", "~> 2.0.1"
+end
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Blog
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
