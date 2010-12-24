4ee9a128eda8dd593ad1c51233530b50cfd94d0d
dv suresh <dv.mymail@gmail.com>
2010-12-24 09:20:18 -0600

    Modified code to take username and email during registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
 end
