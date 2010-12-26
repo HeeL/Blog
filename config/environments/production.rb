028b69ea5c8542bb0d7782febd0a3dae1b23206d
HeeL <parizhskiy@gmail.com>
2010-12-26 13:15:37 +0200

    devise production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Blog::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  
+  config.action_mailer.default_url_options = { :host => 'rails3blog.heroku.com' }
 end
