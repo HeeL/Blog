97a471f9bb24facc1c0b6364f1da8e356502f884
HeeL <parizhskiy@gmail.com>
2011-01-02 23:30:18 +0200

    minor style things

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
 
-  validates :name, :presence => true, :length => { :within => 5..100 }
-  validates :text, :presence => true, :length => { :within => 10..1000 }
+  validates :name, :presence => true, :length => { :within => 3..100 }
+  validates :text, :presence => true, :length => { :within => 5..1000 }
   validates :email, :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }  
             
   belongs_to :post
